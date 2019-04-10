a5a6a05d7a5f2220d59dcbe576147c8f3d784b4d
sowmyan1 <49471342+sowmyan1@users.noreply.github.com>
2019-04-10 16:46:19 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,28 +1,11 @@
 #include <stdio.h>
+ 
 int main()
 {
-    int n=5, i, flag = 0;
-    
-    for(i = 2; i <= n/2; ++i)
-    {
-        if(n%i == 0)
-        {
-            flag = 1;
-            break;
-        }
-    }
-
-    if (n == 1) 
-    {
-      printf("1 is neither a prime nor a composite number.");
-    }
-    else 
-    {
-        if (flag == 0)
-          printf("%d is a prime number.", n);
-        else
-          printf("%d is not a prime number.", n);
-    }
-    
-    return 0;
+  int c, n=5, fact = 1;
+ 
+  for (c = 1; c <= n; c++)
+    fact = fact * c;
+ 
+  return fact;
 }
