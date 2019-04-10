a12f70c7ea65f216b349d6101987234994e55439
sowmyan1 <49471342+sowmyan1@users.noreply.github.com>
2019-04-10 16:24:55 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,57 +1,28 @@
-/*
- */
-
-#include <avr/io.h>
-#include <avr/delay.h>
-#include <avr/interrupt.h>
-
-volatile int Flag1=0, Flag2=0;
-int main(void)
+#include <stdio.h>
+int main()
 {
-    DDRD&=~(1<<2);
-    DDRD&=~(1<<3);
-    PORTD|=(1<<2);
-    PORTD|=(1<<3);
-
-    DDRB|=(1<<PB0);
-    DDRB|=(1<<PB1);
-    EICRA|=(1<<ISC00);
-    EICRA|=(1<<ISC10);
-    EIMSK|=(1<<INT0);
-    EIMSK|=(1<<INT1);
-    sei();
-
-    while(1)
+    int n=5, i, flag = 0;
+    
+    for(i = 2; i <= n/2; ++i)
     {
-        if(Flag1==1)
+        if(n%i == 0)
         {
-         PORTB|=(1<<0);
-         _delay_ms(2000);
-         Flag1=0;
-        }
-        else
-        {
-         PORTB&=~(1<<0);
-        }
-
-        if(Flag2==1)
-        {
-         PORTB|=(1<<1);
-         _delay_ms(2000);
-         Flag2=0;
-        }
-        else
-        {
-         PORTB&=~(1<<1);
+            flag = 1;
+            break;
         }
     }
+
+    if (n == 1) 
+    {
+      printf("1 is neither a prime nor a composite number.");
+    }
+    else 
+    {
+        if (flag == 0)
+          printf("%d is a prime number.", n);
+        else
+          printf("%d is not a prime number.", n);
+    }
+    
     return 0;
-}
-ISR(INT0_vect)
-{
-    Flag1=1;
-}
-ISR(INT1_vect)
-{
-    Flag2=1;
 }
